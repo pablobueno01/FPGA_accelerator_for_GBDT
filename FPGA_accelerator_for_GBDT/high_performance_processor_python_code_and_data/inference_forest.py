55293db862258b6c6925a2e19a8758fe86a9f7cf
pablobueno01 <799101@unizar.es>
2024-06-19 14:07:25 +0200

    Comienzo fichero para bosque

--- FPGA_accelerator_for_GBDT/high_performance_processor_python_code_and_data/inference_forest.py
+++ FPGA_accelerator_for_GBDT/high_performance_processor_python_code_and_data/inference_forest.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+from inference_reduced import *
 
 def main():
-    print("Hello, World!")
+    # For each image
+    for img in IMAGES:
+        
+        # Get image information
+        image_info = IMAGES[img]
+        image_name = image_info["key"]
+        train_size = image_info["p"]
+        
+        print("\n----------------{}----------------".format(image_name))
+        
+        # Load image
+        X, y = load_image(image_info)
+        
+        # Preprocess image
+        X, y = pixel_classification_preprocessing(X, y)
+        
+        # Separate data into train and test sets
+        X_train, y_train, X_test, y_test = separate_pixels(X, y, train_size)
+        print("Train pixels: {}\tTest pixels: {}".format(X_train.shape[0], X_test.shape[0]))
 
 if __name__ == "__main__":
     main()
