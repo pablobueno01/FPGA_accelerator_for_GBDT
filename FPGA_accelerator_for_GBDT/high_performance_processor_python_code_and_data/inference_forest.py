857a2e496b0a2feb8e38bea64096e62eaea8a5ab
pablobueno01 <799101@unizar.es>
2024-06-20 13:30:31 +0200

    Primer bosque

--- FPGA_accelerator_for_GBDT/high_performance_processor_python_code_and_data/inference_forest.py
+++ FPGA_accelerator_for_GBDT/high_performance_processor_python_code_and_data/inference_forest.py
@@ -2,6 +2,48 @@
 # -*- coding: utf-8 -*-
 from inference_reduced import *
 
+FOREST = [
+    LGBMClassifier(random_state=69),
+    LGBMClassifier(random_state=69, max_depth=5),
+    LGBMClassifier(random_state=69, n_estimators=50),
+    LGBMClassifier(random_state=69, n_estimators=150),
+    LGBMClassifier(random_state=69, num_leaves=11),
+    LGBMClassifier(random_state=69, num_leaves=51),
+    LGBMClassifier(random_state=69, min_child_samples=10),
+    LGBMClassifier(random_state=69, min_child_samples=30),
+    LGBMClassifier(random_state=69, subsample_freq=1, subsample=0.8),
+    LGBMClassifier(random_state=69, subsample_freq=1, subsample=0.6),
+    LGBMClassifier(random_state=69, subsample_freq=2, subsample=0.8),
+    LGBMClassifier(random_state=69, subsample_freq=2, subsample=0.6),
+    LGBMClassifier(random_state=69, learning_rate=0.05),
+    LGBMClassifier(random_state=69, learning_rate=0.2),
+    LGBMClassifier(random_state=69, reg_alpha=0.1),
+    LGBMClassifier(random_state=69, reg_lambda=0.1)
+]
+
+def forest_predict(trained_forest, X_test, y_test):
+    individual_accuracies = []
+    sum_probabilities = None
+    for clf in trained_forest:
+        # Get accuracy of the individual classifier
+        accuracy = clf.score(X_test, y_test)
+        individual_accuracies.append(accuracy)
+        # Get probabilities of each class for the individual classifier
+        probabilities = clf.predict_proba(X_test)
+        if sum_probabilities is None:
+            sum_probabilities = probabilities
+        else:
+            sum_probabilities += probabilities
+    
+    # Get the probabilities of the forest as the average of the individual probabilities
+    forest_probabilities = sum_probabilities / len(trained_forest)
+    # Get the accuracy of the forest
+    forest_predictions = np.argmax(forest_probabilities, axis=1)
+    test_ok = forest_predictions == y_test
+    accuracy_forest = np.mean(test_ok)
+    
+    return accuracy_forest, individual_accuracies
+
 def main():
     # For each image
     for img in IMAGES:
@@ -32,14 +74,25 @@ def main():
 
         # Load the trained reduced model
         model = joblib.load("{}/{}_model_{}.joblib".format(models_dir, image_name, k))
-        accuracy_cv = cross_val_accuracy(model, X_train_k, y_train)
         print("\nReduced model with {} features:".format(k))
-        print("Cross-val Accuracy:  {:.3f}".format(accuracy_cv))
 
         # Perform inference with reduced model
-        time, speed, accuracy_test = lightgbm_predict(model, X_test_k, y_test)
-        print("Test Accuracy:   {:.3f}".format(accuracy_test))
-        print("Prediction time: {:.3f}s ({}px/s)\n".format(time, speed))
+        _, _, accuracy_test = lightgbm_predict(model, X_test_k, y_test)
+        print("Test Accuracy:   {:.3f}\n".format(accuracy_test))
 
+        # Train forest
+        for clf in FOREST:
+            clf.fit(X_train_k, y_train)
+
+        # Perform inference with forest
+        accuracy_forest, individual_accuracies = forest_predict(FOREST, X_test_k, y_test)
+        print("Individual Test Accuracies:")
+        for accuracy in individual_accuracies:
+            print("{:.3f}".format(accuracy))
+        print("Forest Test Accuracy: {:.3f}".format(accuracy_forest))
+
+        # Save forest models
+        joblib.dump(FOREST, "{}/forest_models.joblib".format(models_dir))
+        
 if __name__ == "__main__":
     main()
