e53c58ed2db3ec21ef02988cada50eea6624f8e6
pablobueno01 <799101@unizar.es>
2024-08-03 13:54:39 +0200

    Formato dos2unix de scripts y ejecutado para clase 0

--- FPGA_accelerator_for_GBDT/FPGA_VHDL_code_and_data/sim/select_classes.sh
+++ FPGA_accelerator_for_GBDT/FPGA_VHDL_code_and_data/sim/select_classes.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
 # $1 --> selected class num
+# $2 --> file
 
-# Array of files
-files=("IP_test.vhd" "KSC_test.vhd" "PU_test.vhd" "SV_test.vhd")
+# Start and end of the pixels of every class
+start=$(grep -n "PIXELS OF CLASS" "$2" | grep " 0$" | cut -d ":" -f 1)
+end=$(($(grep -n "wait;" "$2" | cut -d ":" -f 1) - 1))
 
-# Iterate over the files
-for file in "${files[@]}"
-do
-    # Start and end of the pixels of every class
-    start=$(grep -n "PIXELS OF CLASS" "$file" | grep " 0$" | cut -d ":" -f 1)
-    end=$(($(grep -n "wait;" "$file" | cut -d ":" -f 1) - 1))
+# Remove the coments for every class
+sed -i ''${start}','${end}' s/^--//' "$2"
 
-    # Remove the coments for every class
-    sed -i ''${start}','${end}' s/^--//' "$file"
+# Comment every class
+sed -i ''${start}','${end}' s/^/--/' "$2"
 
-    # Comment every class
-    sed -i ''${start}','${end}' s/^/--/' "$file"
+# Start and end of the pixels of the selected class
+start=$(grep -n "PIXELS OF CLASS" "$2" | grep " $1$" | cut -d ":" -f 1)
+new_end=$(grep -n "PIXELS OF CLASS" "$2" | grep " $(($1 + 1))$" | cut -d ":" -f 1)
+if [ "$new_end" ]
+then
+    end=$new_end
+fi
 
-    # Start and end of the pixels of the selected class
-    start=$(grep -n "PIXELS OF CLASS" "$file" | grep " $1$" | cut -d ":" -f 1)
-    new_end=$(grep -n "PIXELS OF CLASS" "$file" | grep " $(($1 + 1))$" | cut -d ":" -f 1)
-    if [ "$new_end" ]
-    then
-        end=$new_end
-    fi
-
-    # Remove the coments for the selected class
-    sed -i ''${start}','${end}' s/^--//' "$file"
-done
+# Remove the coments for the selected class
+sed -i ''${start}','${end}' s/^--//' "$2"
 
