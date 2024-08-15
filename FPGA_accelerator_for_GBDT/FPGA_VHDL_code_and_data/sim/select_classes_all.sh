ba8bb6fa0869c6e2008c0d2d0cc7cf6c60ef5e41
pablobueno01 <799101@unizar.es>
2024-08-15 13:43:42 +0200

    Proyecto normal funcional con todas las clases

--- FPGA_accelerator_for_GBDT/FPGA_VHDL_code_and_data/sim/select_classes_all.sh
+++ FPGA_accelerator_for_GBDT/FPGA_VHDL_code_and_data/sim/select_classes_all.sh
@@ -3,7 +3,7 @@
 # $1 --> selected class num
 
 # Check if the argument is valid
-if ! [[ $1 =~ ^[0-9]+$ ]]; then
+if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
     echo "Invalid argument. Exiting."
     exit 1
 fi
@@ -18,23 +18,27 @@ do
     start=$(grep -n "PIXELS OF CLASS" "$file" | grep " 0$" | cut -d ":" -f 1)
     end=$(($(grep -n "wait;" "$file" | cut -d ":" -f 1) - 1))
 
-    # Remove the coments for every class
+    # Remove the comments for every class
     sed -i ''${start}','${end}' s/^--//' "$file"
 
-    # Comment every class
-    sed -i ''${start}','${end}' s/^/--/' "$file"
+    if [ $1 -ge 0 ]; then
+        # Comment every class
+        sed -i ''${start}','${end}' s/^/--/' "$file"
+        # Start and end of the pixels of the selected class
+        start=$(grep -n "PIXELS OF CLASS" "$file" | grep " $1$" | cut -d ":" -f 1)
+        new_end=$(grep -n "PIXELS OF CLASS" "$file" | grep " $(($1 + 1))$" | cut -d ":" -f 1)
+        if [ "$new_end" ]
+        then
+            end=$new_end
+        fi
 
-    # Start and end of the pixels of the selected class
-    start=$(grep -n "PIXELS OF CLASS" "$file" | grep " $1$" | cut -d ":" -f 1)
-    new_end=$(grep -n "PIXELS OF CLASS" "$file" | grep " $(($1 + 1))$" | cut -d ":" -f 1)
-    if [ "$new_end" ]
-    then
-        end=$new_end
+        # Remove the comments for the selected class
+        sed -i ''${start}','${end}' s/^--//' "$file"
+
+        echo "Selected class $1 in $file"
+    else
+        echo "Selected every class in $file"
     fi
 
-    # Remove the coments for the selected class
-    sed -i ''${start}','${end}' s/^--//' "$file"
-
-    echo "Selected class $1 in $file"
 done
 
